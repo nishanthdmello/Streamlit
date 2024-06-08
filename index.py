484dbdc24c338d88b1458d678810d87f11e827dd
Nishanth <nishanthdmello2003@gmail.com>
2024-06-08 17:15:51 +0530

    small changes

--- index.py
+++ index.py
@@ -56,7 +56,7 @@ def schedule_email_on_date(date, name):
         if datetime.now().strftime("%Y-%m-%d") == date:
             send_email(name)
 
-    schedule.every().day.at("16:51").do(job).tag(date)
+    schedule.every().day.at("17:17").do(job).tag(date)
 
 for date, name in zip(dates, names):
     schedule_email_on_date(date, name)
